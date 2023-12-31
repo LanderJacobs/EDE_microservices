91f3fb16a305a3fbce04629a7a44fd0829622e80
r0778068 <r0778068@student.thomasmore.be>
2023-12-31 16:11:15 +0100

    accidentally made it in gradle instead of maven

--- content-service/src/main/java/fact/it/contentservice/repository/ContentRepository.java
+++
@@ -1,8 +0,0 @@
-package fact.it.contentservice.repository;
-
-import fact.it.contentservice.model.Content;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContentRepository extends MongoRepository<Content, String> {
-    Content findFirstByRoomName(String roomName);
-}
